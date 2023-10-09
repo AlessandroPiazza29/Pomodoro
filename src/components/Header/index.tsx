309deb2f273316df971958469e5ae8139420ca73
Alessandro Piazza <alessandro.piazzati@gmail.com>
2023-10-08 22:42:07 -0300

    Header e body

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+    return <HeaderContainer>
+        <span>Logo</span>
+            <nav>
+                
+                <a >Timer</a>
+                <a >History</a>
+            </nav>
+            </HeaderContainer>
+}
